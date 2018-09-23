47212914092c25c7e44628db4cf7b8ccc577fcfa
Pathe SENE <pathe.sene@gmail.com>
2018-09-23 14:41:51 +0000

    Add model zoo

---
+++ app/models/zoo.rb
@@ -0,0 +1,5 @@
+class Zoo < ApplicationRecord
+  validates :name,
+    presence: true,
+    length:   { maximum: 255 }
+end
---
+++ spec/models/zoo_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe Zoo, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_most(255) }
+end
