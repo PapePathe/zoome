1d55e5342fcef337f0e200ae1da97bd43bdb4306
Pathe SENE <pathe.sene@gmail.com>
2018-09-26 19:26:57 +0000

    Annotate factories.

--- spec/factories/animal_keepers.rb
+++ spec/factories/animal_keepers.rb
@@ -6,3 +6,26 @@ FactoryBot.define do
     birth_date { "2018-09-24" }
   end
 end
+
+# == Schema Information
+# Schema version: 20180926191222
+#
+# Table name: animal_keepers
+#
+#  id         :uuid             not null, primary key
+#  birth_date :date
+#  first_name :string
+#  job        :string
+#  last_name  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  zoo_id     :uuid
+#
+# Indexes
+#
+#  index_animal_keepers_on_zoo_id  (zoo_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (zoo_id => zoos.id)
+#
--- spec/factories/zoos.rb
+++ spec/factories/zoos.rb
@@ -3,3 +3,23 @@ FactoryBot.define do
     name { "Parc Hann" }
   end
 end
+
+# == Schema Information
+# Schema version: 20180926191222
+#
+# Table name: zoos
+#
+#  id         :uuid             not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  manager_id :uuid
+#
+# Indexes
+#
+#  index_zoos_on_manager_id  (manager_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (manager_id => managers.id)
+#
