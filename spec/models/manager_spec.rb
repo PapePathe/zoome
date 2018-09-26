29ad9ce7fb9c3ed02f2c152843283ce9419d2e95
Pathe SENE <pathe.sene@gmail.com>
2018-09-26 19:07:58 +0000

    Delete blank line.

--- spec/models/manager_spec.rb
+++ spec/models/manager_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Manager, type: :model do
-
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_most(255) }
   it { is_expected.to have_many(:zoos) }
