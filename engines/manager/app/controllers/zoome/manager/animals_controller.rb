0852036940f8873f12ef49553c2addf96c0f9416
Pathe SENE <pathe.sene@gmail.com>
2018-09-28 12:22:27 +0000

    Add controller animals in manager.

---
+++ engines/manager/app/controllers/zoome/manager/animals_controller.rb
@@ -0,0 +1,8 @@
+require_dependency "zoome/manager/application_controller"
+
+module Zoome::Manager
+  class AnimalsController < ApplicationController
+    def index
+    end
+  end
+end
--- engines/manager/config/routes.rb
+++ engines/manager/config/routes.rb
@@ -1,10 +1,11 @@
 Zoome::Manager::Engine.routes.draw do
   root to: 'dashboard#index'
 
-  delete '/deconnexion',    to: 'sessions#destroy', as: :deconnexion
-  get    '/connexion',      to: 'sessions#new',     as: :connexion
-  post   '/connexion',      to: 'sessions#create'
+  delete '/deconnexion', to: 'sessions#destroy', as: :deconnexion
+  get    '/connexion',   to: 'sessions#new',     as: :connexion
+  post   '/connexion',   to: 'sessions#create'
 
   resources :doctors
+  resources :animals
   resources :animal_keepers
 end
